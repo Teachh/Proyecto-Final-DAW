fee0b1c7cdd57badb57868dfb2133a47b776917d
Héctor <50625867+Teachh@users.noreply.github.com>
2020-05-02 11:11:26 +0200

    Creacion plantilla cuadno se esta registado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,57 +1,5 @@
-@extends('layouts.app')
-@section('css')
-<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap" rel="stylesheet">
-
-<link rel="stylesheet" href="{{asset('css/home/open-iconic-bootstrap.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/home/animate.css')}}">
-
-<link rel="stylesheet" href="{{asset('css/home/owl.carousel.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/home/owl.theme.default.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/home/magnific-popup.css')}}">
-
-<link rel="stylesheet" href="{{asset('css/home/aos.css')}}">
-
-<link rel="stylesheet" href="{{asset('css/home/ionicons.min.css')}}">
-
-<link rel="stylesheet" href="{{asset('css/home/bootstrap-datepicker.css')}}">
-<link rel="stylesheet" href="{{asset('css/home/jquery.timepicker.css')}}">
-
-
-<link rel="stylesheet" href="{{asset('css/home/flaticon.css')}}">
-<link rel="stylesheet" href="{{asset('css/home/icomoon.css')}}">
-<link rel="stylesheet" href="{{asset('css/home/style.css')}}">
-<style>
-    header,
-    footer {
-        display: none;
-    }
-
-    #colorlib-aside {
-        background: #5C746A;
-    }
-
-</style>
-@endsection
-@section('content')
-<div id="colorlib-page">
-    <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
-    <aside id="colorlib-aside" role="complementary" class="js-fullheight">
-        <img src="{{asset('img/logo.png')}}" id="logomain" alt="">
-        <nav id="colorlib-main-menu" role="navigation">
-            <ul>
-                <li class="colorlib-active"><a href="index.html">Inicio</a></li>
-                <li><a href="{{action('UserController@show', auth()->user()->id)}}">Perfil</a></li>
-                <li><a href="travel.html">Dibujo libre</a></li>
-                <li>                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a></li>
-            </ul>
-        </nav>
-    </aside> <!-- END COLORLIB-ASIDE -->
-
+@extends('layouts.logged')
+@section('content2')
     <div id="colorlib-main">
         <section class="ftco-section ftco-no-pt ftco-no-pb">
             <div class="container">
@@ -121,7 +69,7 @@
                             </form>
                         </div>
 						<!-- !Buscador -->
-						<!-- EN UN FUTURO SE PUEDE MIRAR
+						<!-- EN UN FUTURO SE PUEDE MIRAR SI HAGO CATEGORIAS	
                         <div class="sidebar-box ftco-animate">
                             <h3 class="sidebar-heading">Categories</h3>
                             <ul class="categories">
@@ -149,8 +97,8 @@
 								@endphp
 								
 								<div class="block-21 mb-4 d-flex">
-                                <a class="blog-img mr-4" style="background-image: url({{$actualUser->profile_picture}});"></a>
-                                <div class="text">
+                                <a class="blog-img" style="background-image: url({{$actualUser->profile_picture}});"></a>
+                                <div class="text pl-4" style="background:linear-gradient(rgba(255,255,255,.9), rgba(255,255,255,.7)), url({{$actualUser->background}});background-position: center top;background-size: contain;">
                                     <h3 class="heading"><a href="{{action('UserController@show', $actualUser->id)}}">{{$actualUser->name}}</a></h3>
                                     <div class="meta">
 										<span><i class="fas fa-users"></i> {{$actualFollow}}</span>
@@ -165,21 +113,19 @@
                         <div class="sidebar-box ftco-animate">
                             <h3 class="sidebar-heading">Ordenar por</h3>
                             <ul class="tagcloud">
-                                <a href="#" class="tag-cloud-link">animals</a>
-                                <a href="#" class="tag-cloud-link">human</a>
-                                <a href="#" class="tag-cloud-link">people</a>
-                                <a href="#" class="tag-cloud-link">cat</a>
-                                <a href="#" class="tag-cloud-link">dog</a>
-                                <a href="#" class="tag-cloud-link">nature</a>
-                                <a href="#" class="tag-cloud-link">leaves</a>
-                                <a href="#" class="tag-cloud-link">food</a>
+                                <a href="#" class="tag-cloud-link">Votos positivos</a>
+                                <a href="#" class="tag-cloud-link">Votos negativos</a>
+                                <a href="#" class="tag-cloud-link">Más seguidores</a>
+                                <a href="#" class="tag-cloud-link">Menos seguidores</a>
+                                <a href="#" class="tag-cloud-link">Más nuevo</a>
+                                <a href="#" class="tag-cloud-link">Más antiguo</a>
                             </ul>
                         </div>
 						<!-- !Ordenar por lo que ponga-->
 						<!-- SI ME SOBRA TIEMPO ORDENAR POR TIEMPO TAMBIEN -->
                         <div class="sidebar-box ftco-animate">
 						SI ME SOBRA TIEMPO ORDENAR POR TIEMPO TAMBIEN
-                            <h3 class="sidebar-heading">Archives</h3>
+                            <h3 class="sidebar-heading">Fechas de dibujos</h3>
                             <ul class="categories">
                                 <li><a href="#">Decob14 2018 <span>(10)</span></a></li>
                                 <li><a href="#">September 2018 <span>(6)</span></a></li>
@@ -196,24 +142,4 @@
         </section>
     </div><!-- END COLORLIB-MAIN -->
 </div><!-- END COLORLIB-PAGE -->
-
-<!-- loader -->
-<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
-        <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
-        <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /></svg></div>
-<script src="{{asset('js/home/jquery.min.js')}}"></script>
-<script src="{{asset('js/home/jquery-migrate-3.0.1.min.js')}}"></script>
-<script src="{{asset('js/home/popper.min.js')}}"></script>
-<script src="{{asset('js/home/bootstrap.min.js')}}"></script>
-<script src="{{asset('js/home/jquery.easing.1.3.js')}}"></script>
-<script src="{{asset('js/home/jquery.waypoints.min.js')}}"></script>
-<script src="{{asset('js/home/jquery.stellar.min.js')}}"></script>
-<script src="{{asset('js/home/owl.carousel.min.js')}}"></script>
-<script src="{{asset('js/home/jquery.magnific-popup.min.js')}}"></script>
-<script src="{{asset('js/home/aos.js')}}"></script>
-<script src="{{asset('js/home/jquery.animateNumber.min.js')}}"></script>
-<script src="{{asset('js/home/scrollax.min.js')}}"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-<script src="{{asset('js/home/google-map.js')}}"></script>
-<script src="{{asset('js/home/main.js')}}"></script>
 @endsection
---
+++ resources/views/layouts/logged.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.app')
+@section('css')
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="{{asset('css/home/open-iconic-bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/home/animate.css')}}">
+
+<link rel="stylesheet" href="{{asset('css/home/owl.carousel.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/home/owl.theme.default.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/home/magnific-popup.css')}}">
+
+<link rel="stylesheet" href="{{asset('css/home/aos.css')}}">
+
+<link rel="stylesheet" href="{{asset('css/home/ionicons.min.css')}}">
+
+<link rel="stylesheet" href="{{asset('css/home/bootstrap-datepicker.css')}}">
+<link rel="stylesheet" href="{{asset('css/home/jquery.timepicker.css')}}">
+
+
+<link rel="stylesheet" href="{{asset('css/home/flaticon.css')}}">
+<link rel="stylesheet" href="{{asset('css/home/icomoon.css')}}">
+<link rel="stylesheet" href="{{asset('css/home/style.css')}}">
+<style>
+    header,
+    footer {
+        display: none;
+    }
+
+    #colorlib-aside {
+        background: #5C746A;
+    }
+
+</style>
+@endsection
+@section('content')
+<div id="colorlib-page">
+    <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+    <aside id="colorlib-aside" role="complementary" class="js-fullheight">
+        <img src="{{asset('img/logo.png')}}" id="logomain" alt="">
+        <nav id="colorlib-main-menu" role="navigation">
+            <ul>
+                <li class="colorlib-active"><a href="index.html">Inicio</a></li>
+                <li><a href="{{action('UserController@show', auth()->user()->id)}}">Perfil</a></li>
+                <li><a href="travel.html">Dibujo libre</a></li>
+                <li>                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a></li>
+            </ul>
+        </nav>
+    </aside> <!-- END COLORLIB-ASIDE -->
+<!-- CONTENIDO -->
+@yield('content2')
+<!-- !CONTENIDO -->
+</div><!-- END COLORLIB-PAGE -->
+
+<!-- loader -->
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+        <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+        <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /></svg></div>
+<script src="{{asset('js/home/jquery.min.js')}}"></script>
+<script src="{{asset('js/home/jquery-migrate-3.0.1.min.js')}}"></script>
+<script src="{{asset('js/home/popper.min.js')}}"></script>
+<script src="{{asset('js/home/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/home/jquery.easing.1.3.js')}}"></script>
+<script src="{{asset('js/home/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('js/home/jquery.stellar.min.js')}}"></script>
+<script src="{{asset('js/home/owl.carousel.min.js')}}"></script>
+<script src="{{asset('js/home/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset('js/home/aos.js')}}"></script>
+<script src="{{asset('js/home/jquery.animateNumber.min.js')}}"></script>
+<script src="{{asset('js/home/scrollax.min.js')}}"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+<script src="{{asset('js/home/google-map.js')}}"></script>
+<script src="{{asset('js/home/main.js')}}"></script>
+@endsection
